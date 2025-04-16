a5da09d18d6ec828b63743f7b35a8cfcb6df4904
Giovanna <giovanna.vasques1101@hotmail.com>
2025-04-15 22:36:34 -0300

    Adicionando lista de itens e exibindo + botao de realizar pedido

--- lib/menu.dart
+++ lib/menu.dart
@@ -1,5 +1,8 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:pizza_app/menu_item.dart';
+import 'package:pizza_app/pizza_data.dart';
 
 class Menu extends StatelessWidget {
   const Menu({super.key});
@@ -7,21 +10,26 @@ class Menu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar (
-        title: Text ("Pizza Menu"),
-      ),
-      body: Column (
-        children: [
-          MenuItem(img: "images/focaccia.jpg", nome: "Foccacia", descricao: "Texto2", valor: "Texto3"),
-          MenuItem(img: "images/funghi.jpg", nome: "Funghi", descricao: "Texto2", valor: "Texto3"),
-          MenuItem(img: "images/margherita.jpg", nome: "Margherita", descricao: "Texto2", valor: "Texto3"),
-          MenuItem(img: "images/prosciutto.jpg", nome: "Prosciutto", descricao: "Texto2", valor: "Texto3"),
-          MenuItem(img: "images/salamino.jpg", nome: "Salamino", descricao: "Texto2", valor: "Texto3"),
-          MenuItem(img: "images/spinaci.jpg", nome: "Spinaci", descricao: "Texto2", valor: "Texto3")
-        ],
-      )
-    );
+        appBar: AppBar(
+          title: Text("Pizza Menu"),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children:
+                      pizzaData.map((pizza) => MenuItem(pizza: pizza)).toList(),
+                ),
+              ),
+            ),
+            if (isOpen())
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child:
+                    ElevatedButton(onPressed: () {}, child: Text("Order now!")),
+              )
+          ],
+        ));
   }
 }
-
-
--- lib/menu_item.dart
+++ lib/menu_item.dart
@@ -1,51 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:pizza_app/pizza_data.dart';
 
 class MenuItem extends StatelessWidget {
-  const MenuItem({super.key, required this.img, required this.nome, required this.descricao, required this.valor});
+  const MenuItem({super.key, required this.pizza});
 
-  final String img;
-  final String nome;
-  final String descricao;
-  final String valor;
+  final Pizza pizza;
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Row (
+        child: Row(
           children: [
-            Image.asset(
-                img, 
+            ColorFiltered(
+              colorFilter:
+              pizza.soldOut 
+              ?  ColorFilter.mode(Colors.grey, BlendMode.saturation) 
+              : ColorFilter.mode(Colors.transparent, BlendMode.saturation),
+              child: Image.asset(
+                "images/${pizza.photoName}",
                 width: 100,
                 height: 100,
               ),
+            ),
             SizedBox(width: 16),
             Expanded(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    nome,
-                    style: TextStyle (
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold
-                    )
-                  ),
+                    pizza.name,
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold)
+                          ),
                   Text(
-                    descricao,
-                    style: TextStyle (
-                      fontSize: 16,
-                      color: Colors.black87
-                    )
-                  ),
+                    pizza.ingredients,
+                      style: TextStyle(fontSize: 16, color: Colors.black87)
+                      ),
                   Text(
-                    valor,
-                    style: TextStyle (
-                      fontSize: 14,
-                      color: Colors.black54
-                    )
-                  ),
+                    pizza.soldOut ? "sold out" : "R\$${pizza.price}",
+                      style: TextStyle(fontSize: 14, color: Colors.black54)
+                      ),
                 ],
               ),
             )
